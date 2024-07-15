fdb9f6ba3dda6469c77115058725cf8d1e60b6bc
Tonny Bett <betttonny26@gmail.com>
2024-07-16 02:40:03 +0300

    modified README.md, added a padding style to separate the div and hosted the project in local server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # code--challange-week-3
 code challange week 3
+Project Title: Flatiron Movie Theater
+
+*create an html file, js file, css file and a db.json file which will be our local server
+*Add the tailwind cdn link
+*link the css with html
+
+
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-fetch("http://localhost:3000/films/1")
-  .then(response => response.json())
-  .then(films =>
-    films.map(film => {
-      const title = document.createElement("h1")
-      title.innerHTML = film.title
-      main.appendChild(h1)
-    })
-  );
+fetch ("http://localhost:3000/films")
+.then(response => response.json())
+.then(films => 
+  films.map(film=>{
+  const title= document.createElement("h1")
+  title.innerHTML = film.title
+  main.appendChild(title)
+}) 
+)
